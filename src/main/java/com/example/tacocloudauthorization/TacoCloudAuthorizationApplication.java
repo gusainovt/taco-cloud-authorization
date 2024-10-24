be7a7e49676e75002611b971d07bf43311daaf17
Timur-Gus <timurka24478@gmail.ru>
2024-10-24 15:11:16 +0300

    Initial commit

---
+++ src/main/java/com/example/tacocloudauthorization/TacoCloudAuthorizationApplication.java
@@ -0,0 +1,13 @@
+package com.example.tacocloudauthorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudAuthorizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoCloudAuthorizationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tacocloudauthorization/TacoCloudAuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tacocloudauthorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudAuthorizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
